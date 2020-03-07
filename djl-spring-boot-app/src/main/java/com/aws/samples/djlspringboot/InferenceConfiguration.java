3a30ae24a253d6aff9439b353cb268b4e485ab93
Frank Liu <frankfliu2000@gmail.com>
2020-03-06 21:44:14 -0800

    Use DJL 0.3.0 API for InferenceConfiguration

--- djl-spring-boot-app/src/main/java/com/aws/samples/djlspringboot/InferenceConfiguration.java
+++ djl-spring-boot-app/src/main/java/com/aws/samples/djlspringboot/InferenceConfiguration.java
@@ -12,18 +12,14 @@
  */
 package com.aws.samples.djlspringboot;
 
+import ai.djl.Application;
 import ai.djl.MalformedModelException;
 import ai.djl.inference.Predictor;
 import ai.djl.modality.cv.DetectedObjects;
-import ai.djl.modality.cv.SingleShotDetectionTranslator;
-import ai.djl.modality.cv.transform.Resize;
-import ai.djl.modality.cv.transform.ToTensor;
-import ai.djl.mxnet.zoo.MxModelZoo;
+import ai.djl.repository.zoo.Criteria;
 import ai.djl.repository.zoo.ModelNotFoundException;
+import ai.djl.repository.zoo.ModelZoo;
 import ai.djl.repository.zoo.ZooModel;
-import ai.djl.training.util.ProgressBar;
-import ai.djl.translate.Pipeline;
-import ai.djl.translate.Translator;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,70 +28,48 @@ import org.springframework.context.annotation.ScopedProxyMode;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 @Configuration
 public class InferenceConfiguration {
 
     @Bean
-    public Map<String, String> criteria() {
-        Map<String, String> criteria = new ConcurrentHashMap<>();
-        criteria.put("size", "512");
-        criteria.put("backbone", "resnet50");
-        criteria.put("flavor", "v1");
-        criteria.put("dataset", "voc");
-        return criteria;
+    public Criteria<BufferedImage, DetectedObjects> criteria() {
+        return Criteria.builder()
+                .setTypes(BufferedImage.class, DetectedObjects.class)
+                .optApplication(Application.CV.OBJECT_DETECTION)
+                .optFilter("size", "512")
+                .optFilter("backbone", "mobilenet1.0")
+                .optFilter("dataset", "voc")
+                .optArgument("threshold", 0.1)
+                .build();
     }
 
     @Bean
-    public ZooModel<BufferedImage, DetectedObjects> model(@Qualifier("criteria") Map<String, String> criteria)
+    public ZooModel<BufferedImage, DetectedObjects> model(
+            @Qualifier("criteria") Criteria<BufferedImage, DetectedObjects> criteria)
             throws MalformedModelException, ModelNotFoundException, IOException {
-        return MxModelZoo.SSD.loadModel(criteria, new ProgressBar());
+        return ModelZoo.loadModel(criteria);
     }
 
     /**
      * Scoped proxy is one way to have a predictor configured and closed.
      * @param model
-     * @param translator
      * @return
      */
     @Bean(destroyMethod = "close")
     @Scope(value = "prototype", proxyMode = ScopedProxyMode.INTERFACES)
-    public Predictor<BufferedImage, DetectedObjects> predictor(ZooModel<BufferedImage, DetectedObjects> model,
-                                                               Translator<BufferedImage, DetectedObjects> translator) {
-        return model.newPredictor(translator);
+    public Predictor<BufferedImage, DetectedObjects> predictor(ZooModel<BufferedImage, DetectedObjects> model) {
+        return model.newPredictor();
     }
 
     /**
      * Inject with @Resource or autowired. Only safe to be used in the try with resources.
      * @param model
-     * @param translator
      * @return
      */
     @Bean
-    public Supplier<Predictor<BufferedImage, DetectedObjects>> predictorProvider(ZooModel<BufferedImage, DetectedObjects> model,
-                                                                         Translator<BufferedImage, DetectedObjects> translator) {
-        return () -> model.newPredictor(translator);
-    }
-
-
-    @Bean
-    public Translator<BufferedImage, DetectedObjects> translator() {
-
-        int width = 512;
-        int height = 512;
-        double threshold = 0.1d;
-
-        Pipeline pipeline = new Pipeline();
-        pipeline.add(new Resize(width, height)).add(new ToTensor());
-
-        return SingleShotDetectionTranslator.builder()
-                .setPipeline(pipeline)
-                .setSynsetArtifactName("classes.txt")
-                .optThreshold((float) threshold)
-                .optRescaleSize(width, height)
-                .build();
+    public Supplier<Predictor<BufferedImage, DetectedObjects>> predictorProvider(ZooModel<BufferedImage, DetectedObjects> model) {
+        return model::newPredictor;
     }
 }
