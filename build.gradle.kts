6336f96eea67bbdfceb914aafdffa9e5cd18fa1f
Dennis Kieselhorst <mail@dekies.de>
2021-07-01 14:11:50 +0200

    update other dependencies and plugins to latest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import java.io.*;
 
 plugins {
     java
-    id("org.springframework.boot") version "2.3.3.RELEASE" apply false
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    id ("com.google.cloud.tools.jib") version "2.1.0" apply false
-    kotlin("jvm") version "1.3.61" apply false
-    kotlin("plugin.spring") version "1.3.61" apply false
+    id("org.springframework.boot") version "2.5.2" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id ("com.google.cloud.tools.jib") version "3.1.1" apply false
+    kotlin("jvm") version "1.5.20" apply false
+    kotlin("plugin.spring") version "1.5.20" apply false
 }
 
 repositories {
@@ -32,7 +32,7 @@ subprojects {
 
     dependencyManagement {
         imports {
-            mavenBom("org.springframework:spring-framework-bom:5.2.8.RELEASE")
+            mavenBom("org.springframework:spring-framework-bom:5.3.8")
         }
     }
 }
--- djl-spring-boot-common/build.gradle.kts
+++ djl-spring-boot-common/build.gradle.kts
@@ -1,10 +1,10 @@
 
 dependencies {
-	implementation("org.slf4j:slf4j-api:1.7.30")
+	implementation("org.slf4j:slf4j-api:1.7.31")
 	implementation("org.springframework:spring-core")
 	implementation("org.springframework:spring-context")
-	implementation("com.amazonaws:aws-java-sdk-s3:1.11.714")
-	implementation("com.amazonaws:aws-java-sdk-sts:1.11.714")
+	implementation("com.amazonaws:aws-java-sdk-s3:1.12.15")
+	implementation("com.amazonaws:aws-java-sdk-sts:1.12.15")
 	testImplementation("org.slf4j:slf4j-log4j12")
 }
 
